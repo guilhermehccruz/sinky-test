db969f3345d931de90a3bfc830b7fb47a030980b
guilhermehccruz <guihccruz60@gmail.com>
2025-08-15 01:26:04 -0300

    feat: update api default port

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-	await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3001);
 }
 
 void bootstrap();
